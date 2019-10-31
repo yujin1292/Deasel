48ad46696ee12a15c489551962e55ac6cc1a8a19
yujin_Lee <sofod12@naver.com>
2019-10-31 11:20:27 +0900

    UI 업데이트 1차
    SelectActivity에서 PreviewActivity로
    bitmap 이미지 파일 두개를 byteArray로 압축해서 보내려했으나
    압축해도 충분히 큰 파일이라 하나만 전달가능

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -3,24 +3,11 @@
 
 #include <opencv2/opencv.hpp>
 
-extern "C"
-JNIEXPORT void JNICALL
-Java_jinjin_juju_young_d_1easel_ImageActivity_detectEdgeJNI(JNIEnv *env, jobject thiz,
-                                                            jlong input_image, jlong output_image,
-                                                            jint th1, jint th2) {
-    // TODO: implement detectEdgeJNI()
-    cv::Mat &inputMat = *(cv::Mat *) input_image;
-    cv::Mat &outputMat = *(cv::Mat *) output_image;
 
-    cvtColor(inputMat, outputMat, cv::COLOR_RGB2GRAY);
-    Canny(outputMat, outputMat, th1, th2);
-    bitwise_not(outputMat,outputMat);
-
-}
 
 extern "C"
 JNIEXPORT void JNICALL
-Java_jinjin_juju_young_d_1easel_Image2Activity_detectEdgeJNI(JNIEnv *env, jobject thiz,
+Java_jinjin_juju_young_d_1easel_SelectActivity_detectEdgeJNI(JNIEnv *env, jobject thiz,
                                                              jlong input_image, jlong output_image,
                                                              jint th1, jint th2) {
     // TODO: implement detectEdgeJNI()
@@ -32,4 +19,3 @@ Java_jinjin_juju_young_d_1easel_Image2Activity_detectEdgeJNI(JNIEnv *env, jobjec
     bitwise_not(outputMat,outputMat);
 
 }
-
