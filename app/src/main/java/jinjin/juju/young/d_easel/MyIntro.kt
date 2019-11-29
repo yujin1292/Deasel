e291bb3cfdedb006b57bb36995e903268e5540f7
yujin1292 <sofod12@naver.com>
2019-11-29 22:06:53 +0900

    Final

--- app/src/main/java/jinjin/juju/young/d_easel/MyIntro.kt
+++ app/src/main/java/jinjin/juju/young/d_easel/MyIntro.kt
@@ -16,40 +16,39 @@ class MyIntro : AppIntro2() {
 
 
         val first_fragment = AppIntroFragment.newInstance(
-            "♡원하는 사진을 나만의 감각으로 색칠해 보세요♡",
-            "카메라버튼을 누르고 사진을 촬영하거나 갤러리 버튼을 눌러 원하는 사진을 선택하세요!",
+            "♡ 원하는 사진을 나만의 감각으로 색칠해 보세요 ♡",
+            "카메라버튼을 누르고 사진을 촬영하거나 \n갤러리 버튼을 눌러 원하는 사진을 선택하세요!",
             R.drawable.one_intro ,
-            Color.parseColor(R.color.colorPlum.toString())
+            Color.parseColor("#FFDDA0DD")
         )
         val second_fragment = AppIntroFragment.newInstance(
-            "♡원하는 스케치북을 선택하세요♡",
-            "ㄷ,ㅣ,ㅈ,ㅔ,ㄹ 버튼을 이용해 원하는 도안을 고르세요!",
+            "♡ 원하는 스케치북을 선택하세요 ♡",
+            " ㄷ , ㅣ , ㅈ , ㅔ , ㄹ  버튼을 이용해\n원하는 도안을 고르세요!",
             R.drawable.two_intro,
-            Color.parseColor(R.color.colorPlum.toString())
+            Color.parseColor("#FFDDA0DD")
         )
         val third_fragment = AppIntroFragment.newInstance(
-            "♡Coloring Book에서 그리던 그림을 확인할 수 있어요♡",
+            "♡ Coloring Book에서 그리던 그림을 확인할 수 있어요 ♡",
             "그림을 클릭하면 이어서 그릴 수 있어요!",
             R.drawable.three_intro,
-            Color.parseColor(R.color.colorPlum.toString())
+            Color.parseColor("#FFDDA0DD")
         )
         val fourth_fragment = AppIntroFragment.newInstance(
-            "♡꾹 누르면 삭제할수 있어요♡",
-            "coloring-book에서 선택할 수 있어요!",
+            "♡ 그림을 꾹 누르면 \n 삭제할수 있어요 ♡",
+            "Coloring Book 메뉴 에서 선택할 수 있어요!",
             R.drawable.four_intro,
-            Color.parseColor(R.color.colorPlum.toString())
+            Color.parseColor("#FFDDA0DD")
         )
         val fifth_fragment = AppIntroFragment.newInstance(
-            "♡이제 접근을 허용해주세요♡",
+            "♡ 이제 접근을 허용해주세요 ♡",
             "접근을 허용하지 않으면 D-Easel을 이용할 수 없어요",
             R.drawable.five_intro,
-            Color.parseColor(R.color.colorPlum.toString())
+            Color.parseColor("#FFDDA0DD")
         )
 
 
         // Add your slide's fragments here
         // AppIntro will automatically generate the dots indicator and buttons.
-        addSlide(help_first.newInstance(R.layout.help_first_layout))
         addSlide(first_fragment)
         addSlide(second_fragment)
         addSlide(third_fragment)
@@ -63,8 +62,10 @@ class MyIntro : AppIntro2() {
 
         // OPTIONAL METHODS
 
+
+
         // Override bar/separator color
-        setBarColor(Color.parseColor(R.color.colorThistle.toString()))
+        setBarColor(Color.parseColor("#FFD8BFD8"))
         //setSeparatorColor(Color.parseColor("#2196F3"));
 
         // SHOW or HIDE the statusbar
